d4e189009fc62acba6cb7c00b3bdf4719e383066
Sayad Muhammad Shahzaib <smshahzaibshah@gmail.com>
2020-11-20 00:34:49 +0500

    Navigator Added in the Application

--- App.js
+++ App.js
@@ -13,47 +13,75 @@ const Drawer = createDrawerNavigator();
 function MyDrawer() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Article" component={Article} />
+      <Drawer.Screen name="Home" component={StackNavigator} />
+      <Drawer.Screen name="Drawer 2" component={Drawer2} />
     </Drawer.Navigator>
   );
 }
 
+const Drawer1 = ()=>{
+    return (
+      <View style={styles.container}>
+        <Text>Drawer 1</Text>
+      </View>
+    );
+  }
+  
+  const Drawer2 = ()=>{
+    return (
+      <View style={styles.container}>
+        <Text>Drawer 2</Text>
+      </View>
+    );
+  }
+  const styles = StyleSheet.create({
+    container:{ flex: 1, alignItems: 'center', justifyContent: 'center' },
+  });
+
+
 
 const Stack = createStackNavigator();
+const StackNavigator =()=>{
+  return(
+    <Stack.Navigator 
+    //initialRouteName={"Home"}
+    screenOptions={
+      {
+        headerTitleAlign: "center",
+        headerTintColor: 'black', // change header color
+        headerStyle:{
+        backgroundColor: 'lightblue',
+        },
+        headerRight: () => <Button title="Edit"></Button>
+      }
+    }
+    >
+      <Stack.Screen name="Drawer" component={MyDrawer}/>
+      <Stack.Screen
+      name="Start"
+      component={StartScreen}
+      />
+      <Stack.Screen name="Home" component={HomeScreen} 
+      options={{
+        title: 'Welcome',
+        //headerShown: false,
+        // headerTitleAlign: "center",
+        // headerTintColor: 'black', // change header color
+        // headerStyle:{
+        //   backgroundColor: 'lightblue',
+        // }
+        headerRight: () => {}
+      }}/>
+      <Stack.Screen name="LastScreen" component={LastScreen} />
+    </Stack.Navigator>
+
+  )
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={"Home"}
-      screenOptions={
-        {
-          headerTitleAlign: "center",
-          headerTintColor: 'black', // change header color
-          headerStyle:{
-          backgroundColor: 'lightblue',
-          },
-          headerRight: () => <Button title="Edit"></Button>
-        }
-      }
-      >
-        <Stack.Screen
-        name="Start"
-        component={StartScreen}
-        />
-        <Stack.Screen name="Home" component={HomeScreen} 
-        options={{
-          title: 'Welcome',
-          //headerShown: false,
-          // headerTitleAlign: "center",
-          // headerTintColor: 'black', // change header color
-          // headerStyle:{
-          //   backgroundColor: 'lightblue',
-          // }
-          headerRight: () => {}
-        }}/>
-        <Stack.Screen name="Dashboard" component={StartScreen} />
-      </Stack.Navigator>
+      <MyDrawer />
     </NavigationContainer>
   );
 }
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const startScreen =({navigation , route})=> {
-    const id = route.params.id;
-    const name = route.params.name;
+    const id = route.params.id != undefined ?route.params.id : 0;
+    const name = route.params.name != undefined ?route.params.name : "Nothing";;
+    
     return(
       <View style={styles.container}>
       <Text>Screen 1</Text>
@@ -16,7 +17,7 @@ const startScreen =({navigation , route})=> {
     <Text>Name:{name}</Text>
       <Text>Welcome to My App</Text>
       <Button title="Go to Dashboard"
-        onPress={()=> navigation.navigate('Dashboard') }/>
+        onPress={()=> navigation.navigate("LastScreen") }/>
     </View>
   );
   }
